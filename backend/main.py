3d17065a7cb84895264baeb1521e1cc4c3264c5e
Raymond930103 <raymond2040103@gmail.com>
2025-09-06 10:18:45 +0800

    some fix

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from . import models, database
 from .auth import router as auth_router
 from .matches import router as match_router
@@ -8,6 +8,14 @@ from .robot import router as robot_router
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # or ["*"] during development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(match_router)
